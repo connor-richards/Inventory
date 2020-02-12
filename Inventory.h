8e6c5e106389b8d2a5cb5d8955a50f336be901fc
connor-richards <connor.a.t.richards@gmail.com>
2020-02-12 11:23:25 -0800

    add Inventory class documentation

--- Inventory.h
+++ Inventory.h
@@ -5,13 +5,17 @@
 using std::string;
 using std::ostream;
 
+/* Inventory class to keep track of 
+ * types of inventory items */
 class Inventory
 {
+  /* Private member vars for an item */
   private:
     string m_name;
     float m_price;
     int m_in_stock;
 
+  /* Public member functions for the class */
   public:
     Inventory(string,float,int);
     void sell();
